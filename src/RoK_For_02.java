c52e3cfaa784ee7ef9ec25201c489811222c86b4
ztibi <ztibi.zt@gmail.com>
2022-09-05 20:45:03 +0200

    add slope parameter and refactor drawParallelogram

--- src/RoK_For_02.java
+++ src/RoK_For_02.java
@@ -1,27 +1,11 @@
 public class RoK_For_02 {
     public static void main(String[] args) {
-        drawParallelogram(5,5);
+        drawParallelogram(5, 5, 1);
     }
 
-    public static void drawParallelogram(int width, int height){
-        int row = 0;
-        int indentation = 0;
-        for (int column = 1; column <= width; column++) {
-            if (column == 1) {
-                for (int i = 0; i < indentation; i++ ) {
-                    System.out.print(" ");
-                }
-                indentation++;
-            }
-            System.out.print("#");
-            if (column == width) {
-                System.out.print("\n");
-                column = 0;
-                row++;
-            }
-            if (row == height) {
-                break;
-            }
+    public static void drawParallelogram(int width, int height, int slope) {
+        for (int row = 0; row < height; row++) {
+            System.out.println(" ".repeat(slope * row) + "#".repeat(width));
         }
     }
 }
