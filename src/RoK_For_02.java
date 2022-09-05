db8892f334d3ce835a26f52aa24da77503885265
Balazs886 <balazs.kurtyan@gmail.com>
2022-09-05 20:38:00 +0200

    resend

--- src/RoK_For_02.java
+++ src/RoK_For_02.java
@@ -1,8 +1,12 @@
 public class RoK_For_02 {
     public static void main(String[] args) {
+        drawParallelogram(5,5);
+    }
+
+    public static void drawParallelogram(int width, int height){
         int row = 0;
         int indentation = 0;
-        for (int column = 1; column <= 10; column++) {
+        for (int column = 1; column <= width; column++) {
             if (column == 1) {
                 for (int i = 0; i < indentation; i++ ) {
                     System.out.print(" ");
@@ -10,12 +14,12 @@ public class RoK_For_02 {
                 indentation++;
             }
             System.out.print("#");
-            if (column == 10) {
+            if (column == width) {
                 System.out.print("\n");
                 column = 0;
                 row++;
             }
-            if (row == 5) {
+            if (row == height) {
                 break;
             }
         }
